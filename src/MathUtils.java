79c3572b6e80c38471c16cbb135552a020eea831
Colin Witt <witt20@purdue.edu>
2018-09-25 16:16:49 -0400

    Fixed issues in MathUtils

--- src/MathUtils.java
+++ src/MathUtils.java
@@ -3,7 +3,7 @@ public class MathUtils {
      * divideNumbers -- a / b
      * Divide a by b and return the result
      */
-    public static double divideNumbers(int dividend, int divisor) {
+    public static double divideNumbers(double dividend, double divisor) {
         /* Do some casual division ... Super easy ... Shouldn't be any problems here */
         double res = dividend / divisor;
         /* Return the DEFINITELY CORRECT result */
@@ -17,14 +17,14 @@ public class MathUtils {
     public static double raiseToPower(int base, int exp) {
         /* Set initial result to be just the number */
         int res = base;
-
+        
         /* Determine if the exponent is negative */
         boolean isNegative = false;
         if (exp < 0) {
             exp *= -1;
             isNegative = true;
         }
-
+        exp -= 1;
         /* Multiply the result by the base exp times
             Ex: base = 2, exp = 2
                --> res = 2, then res = 4 */
